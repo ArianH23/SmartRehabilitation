1af8041db8bf0b35f5ef62a27167635c7b03b215
ArianH23 <arian.huapaya@gmail.com>
2023-02-27 17:14:09 +0100

    started softmax

---
+++ 2.MediaPipe_YOLO/nn_tips_softmax.py
@@ -0,0 +1,244 @@
+import torch
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
+import glob as glob
+from torch import topk
+import pandas as pd
+from collections.abc import Iterable
+from torch.utils.data import Dataset, DataLoader
+import itertools
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn import preprocessing
+
+# imports from captum library
+from captum.attr import LayerConductance, LayerActivation, LayerIntegratedGradients
+from captum.attr import IntegratedGradients, DeepLift, GradientShap, NoiseTunnel, FeatureAblation
+
+
+class Data(Dataset):
+    def __init__(self, X, y):
+        self.X = torch.from_numpy(X.astype(np.float32))
+        self.y = torch.from_numpy(y.astype(np.float32))
+        self.len = self.X.shape[0]
+       
+    def __getitem__(self, index):
+        return self.X[index], self.y[index]
+   
+    def __len__(self):
+        return self.len
+
+df = pd.read_csv('results/data_final.csv')
+df = df.drop('col64', axis=1)
+
+le = preprocessing.LabelEncoder()
+le.fit(['none', 'power', 'precision'])
+
+# Tips of the fingers positions
+#   |        X           |           Y          |            Z          |
+l = [0, 4, 8, 12, 16, 20, 21, 25, 29, 33, 37, 41]# 42, 46, 50, 54, 58, 62]
+
+df['col65'] = le.transform(df['col65'])
+
+X = df.iloc[:, l]
+y = df.iloc[:, -1]
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15, random_state=42,shuffle=True )
+
+train_data = Data(np.array(X_train), np.array(y_train))
+train_dataloader = DataLoader(dataset=train_data, batch_size=32, shuffle=True)
+
+test_data = Data(np.array(X_val), np.array(y_val))
+test_dataloader = DataLoader(dataset=test_data, batch_size=32, shuffle=True)
+
+input_dim = 12
+hidden_dim_1 = 64
+hidden_dim_2 = 32
+output_dim = 3
+
+class Net(nn.Module):
+    def __init__(self, input_dim, hidden_dim_1, hidden_dim_2, output_dim):
+        super(Net, self).__init__()
+        self.layer_1 = nn.Linear(input_dim, hidden_dim_1)
+        self.layer_2 = nn.Linear(hidden_dim_1, hidden_dim_2)
+        self.layer_3 = nn.Linear(output_dim)
+       
+    def forward(self, x):
+        x = torch.nn.functional.relu(self.layer_1(x))
+        x = torch.nn.functional.relu(self.layer_2(x))
+        x = self.layer_3(x)
+
+        return x
+       
+model = Net(input_dim, hidden_dim_1, hidden_dim_2, output_dim)
+
+learning_rate = 0.01
+
+loss_fn = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+
+num_epochs = 50
+loss_values = []
+
+
+for epoch in range(num_epochs):
+    for X, y in train_dataloader:
+        # zero the parameter gradients
+        optimizer.zero_grad()
+       
+        # forward + backward + optimize
+        pred = model(X)
+        loss = loss_fn(pred, y.unsqueeze(-1))
+        loss_values.append(loss.item())
+        loss.backward()
+        optimizer.step()
+        print(loss)
+
+
+print("Training Complete")
+
+step = np.linspace(0, 100, 21650)
+
+fig, ax = plt.subplots(figsize=(8,5))
+plt.plot(step, np.array(loss_values))
+plt.title("Step-wise Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.show()
+
+
+y_pred = []
+y_test = []
+
+total = 0
+correct = 0
+
+with torch.no_grad():
+    for X, y in test_dataloader:
+        outputs = model(X)
+        predicted = np.where(outputs < 0.5, 0, 1)
+        predicted = list(itertools.chain(*predicted))
+        y_pred.append(predicted)
+        y_test.append(y)
+        total += y.size(0)
+        correct += (predicted == y.numpy()).sum().item()
+
+print(f'Accuracy of the network on the {len(X_val)} test instances: {100 * correct // total}%')
+
+
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+import seaborn as sns
+
+
+y_pred = list(itertools.chain(*y_pred))
+y_test = list(itertools.chain(*y_test))
+
+
+print(classification_report(y_test, y_pred))
+
+
+cf_matrix = confusion_matrix(y_test, y_pred)
+
+plt.subplots(figsize=(8, 5))
+
+sns.heatmap(cf_matrix, annot=True, cbar=False, fmt="g")
+
+plt.show()
+
+ig = IntegratedGradients(model)
+ig_nt = NoiseTunnel(ig)
+dl = DeepLift(model)
+gs = GradientShap(model)
+fa = FeatureAblation(model)
+
+X_val = torch.tensor(X_val.values).float()
+
+ig_attr_test = ig.attribute(X_val, n_steps=5)
+ig_nt_attr_test = ig_nt.attribute(X_val)
+dl_attr_test = dl.attribute(X_val)
+gs_attr_test = gs.attribute(X_val, torch.tensor(X_train.values).float())
+fa_attr_test = fa.attribute(X_val)
+
+
+# prepare attributions for visualization
+
+feature_names = ['wrist_x', 
+                 'thu_tip_x', 
+                 'ind_tip_x',
+                 'mid_tip_x', 
+                 'ring_tip_x', 
+                 'pinky_tip_x',
+                 'wrist_y', 
+                 'thu_tip_y', 
+                 'ind_tip_y',
+                 'mid_tip_y', 
+                 'ring_tip_y', 
+                 'pinky_tip_y',
+                 'wrist_z', 
+                 'thu_tip_z', 
+                 'ind_tip_z',
+                 'mid_tip_z', 
+                 'ring_tip_z', 
+                 'pinky_tip_z']
+
+
+x_axis_data = np.arange(X_val.shape[1])
+x_axis_data_labels = list(map(lambda idx: feature_names[idx], x_axis_data))
+
+ig_attr_test_sum = ig_attr_test.detach().numpy().sum(0)
+ig_attr_test_norm_sum = ig_attr_test_sum / np.linalg.norm(ig_attr_test_sum, ord=1)
+
+ig_nt_attr_test_sum = ig_nt_attr_test.detach().numpy().sum(0)
+ig_nt_attr_test_norm_sum = ig_nt_attr_test_sum / np.linalg.norm(ig_nt_attr_test_sum, ord=1)
+
+dl_attr_test_sum = dl_attr_test.detach().numpy().sum(0)
+dl_attr_test_norm_sum = dl_attr_test_sum / np.linalg.norm(dl_attr_test_sum, ord=1)
+
+gs_attr_test_sum = gs_attr_test.detach().numpy().sum(0)
+gs_attr_test_norm_sum = gs_attr_test_sum / np.linalg.norm(gs_attr_test_sum, ord=1)
+
+fa_attr_test_sum = fa_attr_test.detach().numpy().sum(0)
+fa_attr_test_norm_sum = fa_attr_test_sum / np.linalg.norm(fa_attr_test_sum, ord=1)
+
+lin_weight = model.layer_1.weight[0].detach().numpy()
+y_axis_lin_weight = lin_weight / np.linalg.norm(lin_weight, ord=1)
+
+width = 0.14
+legends = ['Int Grads', 'Int Grads w/SmoothGrad','DeepLift', 'GradientSHAP', 'Feature Ablation', 'Weights']
+
+plt.figure(figsize=(20, 10))
+
+ax = plt.subplot()
+ax.set_title('Comparing input feature importances across multiple algorithms and learned weights')
+ax.set_ylabel('Attributions')
+
+FONT_SIZE = 16
+plt.rc('font', size=FONT_SIZE)            # fontsize of the text sizes
+plt.rc('axes', titlesize=FONT_SIZE)       # fontsize of the axes title
+plt.rc('axes', labelsize=FONT_SIZE)       # fontsize of the x and y labels
+plt.rc('legend', fontsize=FONT_SIZE - 4)  # fontsize of the legend
+
+ax.bar(x_axis_data, ig_attr_test_norm_sum, width, align='center', alpha=0.8, color='#eb5e7c')
+ax.bar(x_axis_data + width, ig_nt_attr_test_norm_sum, width, align='center', alpha=0.7, color='#A90000')
+ax.bar(x_axis_data + 2 * width, dl_attr_test_norm_sum, width, align='center', alpha=0.6, color='#34b8e0')
+ax.bar(x_axis_data + 3 * width, gs_attr_test_norm_sum, width, align='center',  alpha=0.8, color='#4260f5')
+ax.bar(x_axis_data + 4 * width, fa_attr_test_norm_sum, width, align='center', alpha=1.0, color='#49ba81')
+ax.bar(x_axis_data + 5 * width, y_axis_lin_weight, width, align='center', alpha=1.0, color='grey')
+ax.autoscale_view()
+plt.tight_layout()
+
+ax.set_xticks(x_axis_data + 0.5)
+ax.set_xticklabels(x_axis_data_labels)
+
+plt.legend(legends, loc=3)
+plt.show()
