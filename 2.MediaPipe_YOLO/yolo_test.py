23c5063e0a1145c70fa2a99f99fae1167269e62f
ArianH23 <arian.huapaya@gmail.com>
2023-03-09 16:39:21 +0100

    some images for 'none' class

--- 2.MediaPipe_YOLO/yolo_test.py
+++ 2.MediaPipe_YOLO/yolo_test.py
@@ -1,13 +1,12 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8m.yaml")  # build a new model from scratch
-model = YOLO("yolov8m.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolov8x.yaml")  # build a new model from scratch
+model = YOLO("yolov8x.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="coco128.yaml", epochs=3, imgsz=540)  # train the model
+# model.train(data="coco128.yaml", epochs=3, imgsz=540)  # train the model
 # metrics = model.val()  # evaluate model performance on the validation set
 results = model.predict("frames/precision/precision6784.png", show=True) # # predict on an image
-while(True):
-    pass
-# success = model.export(format="onnx")  # export the model to ONNX format
+print('\n', results)
+# success = model.export(format="onnx")  # export the model to ONNX format()
